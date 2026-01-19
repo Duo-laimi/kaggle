a397a3a352be801583b2e9355beee49a7bdb55fc
supeng <supengscu@qq.com>
2026-01-19 14:38:00 +0800

    not important: relative import

--- aimo3_math/kaggle_evaluation/aimo_3_inference_server.py
+++ aimo3_math/kaggle_evaluation/aimo_3_inference_server.py
@@ -1,8 +1,8 @@
-import kaggle_evaluation.core.templates
+import core.templates
 
 import aimo_3_gateway
 
 
-class AIMO3InferenceServer(kaggle_evaluation.core.templates.InferenceServer):
+class AIMO3InferenceServer(core.templates.InferenceServer):
     def _get_gateway_for_test(self, data_paths=None, *args, **kwargs):
         return aimo_3_gateway.AIMO3Gateway(data_paths)
