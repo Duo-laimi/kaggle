957bbdf39297e31e25fa9baa74929fd11843bac3
supeng <supengscu@qq.com>
2026-01-20 18:44:38 +0800

    feat: 基于unsloth加载模型和分词器

--- aimo3_math/model/base.py
+++ aimo3_math/model/base.py
@@ -1,20 +1,57 @@
-
+import torch
+import torch.nn as nn
+from unsloth import FastLanguageModel
 
 
 class Model:
     """A dummy model."""
 
-    def __init__(self):
-        self._model = None
+    def __init__(
+            self,
+            model_name,
+            dtype=None,
+            max_seq_length=1024,
+            inference_mode: bool = True
+    ):
+        self.model_name = model_name
+        self.model = None
+        self.tokenizer = None
+        self.dtype = dtype
+        self.max_seq_length = max_seq_length
+        self.inference_mode = inference_mode
+        if not inference_mode:
+            self.load()
 
     def load(self):
         """Simulate model loading."""
         print("Loading model...")
-        # Just return a "model" that always answers with 0
-        return lambda problem: 0
+        self.model, self.tokenizer = FastLanguageModel.from_pretrained(
+            model_name=self.model_name,
+            dtype=self.dtype,  # None for auto detection
+            max_seq_length=self.max_seq_length,  # Choose any for long context!
+            load_in_4bit=True,  # 4 bit quantization to reduce memory
+            full_finetuning=False,  # [NEW!] We have full finetuning now!
+        )
+        if not self.inference_mode:
+            self.model = FastLanguageModel.get_peft_model(
+                self.model,
+                r=8,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+                target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
+                                "gate_proj", "up_proj", "down_proj", ],
+                lora_alpha=16,
+                lora_dropout=0,  # Supports any, but = 0 is optimized
+                bias="none",  # Supports any, but = "none" is optimized
+                # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+                use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+                random_state=3407,
+                use_rslora=False,  # We support rank stabilized LoRA
+                loftq_config=None,  # And LoftQ
+            )
+        return self.model
 
+    @torch.no_grad()
     def predict(self, problem: str):
         # Employ lazy loading: load model on the first model.predict call
-        if self._model is None:
-            self._model = self.load()
-        return self._model(problem)
+        if self.model is None:
+            self.model = self.load()
+        return self.model(problem)
