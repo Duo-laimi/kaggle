09b2f7d2e1dc9abb1c6609d3f0c31146549616cb
duolaimi20250409 <duolaimi20250409@163.com>
2026-01-28 13:12:27 +0800

    try to load gpt-oss gguf

--- aimo3_math/test.py
+++ aimo3_math/test.py
@@ -1,9 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_path = "/mnt/workspace/model/gpt-oss-20b"
+gguf_file = "/mnt/workspace/model/gpt-oss-20b-gguf/gpt-oss-20b-Q4_K_M.gguf"
 
 chat_model = AutoModelForCausalLM.from_pretrained(
     model_path,
+    gguf_file=gguf_file,
     # load_in_4bit=True,
     device_map="auto"
 )
